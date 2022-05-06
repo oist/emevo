2c95862fcd34337aba6c8dfcf2da51bd391eb482
kngwyu <yuji.kngw.80s.revive@gmail.com>
2022-05-06 14:39:09 +0900

    Rename enviroment to env

--- src/emevo/__init__.py
+++ src/emevo/__init__.py
@@ -3,7 +3,7 @@ EmEvo is a simulation platform for embodied evolution of robots.
 This package contains API definitions and some environment implementations.
 """
 from emevo.body import Body, Encount, Profile
-from emevo.environment import Environment
+from emevo.env import Env
 from emevo.environments import make, register
 
 __version__ = "0.1.0"
