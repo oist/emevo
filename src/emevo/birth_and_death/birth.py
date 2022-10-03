bbf19fb46572c13c1cfe1bcaeba462012b03274b
kngwyu <yuji.kngw.80s.revive@gmail.com>
2022-10-03 17:50:48 +0900

    GeneralizedLogistic

--- src/emevo/birth_and_death/birth.py
+++ src/emevo/birth_and_death/birth.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 import dataclasses
 from typing import Callable, Protocol, TypeVar
 
-from emevo.birth_and_death.statuses import HasEnergy
+from emevo.birth_and_death.statuses import HasAgeAndEnergy
 
 # typevar for status
 S = TypeVar("S", contravariant=True)
@@ -13,3 +13,9 @@ class BirthFunction(Protocol[S]):
     def __call__(self, status: S) -> float:
         """Birth function b(t)"""
         ...
+
+
+@dataclasses.dataclass
+class GeneralizedLogstic(BirthFunction[HasAgeAndEnergy]):
+    def __call__(self, status: HasAgeAndEnergy) -> float:
+        return super().__call__(status)
