5f13026fe884bf15e3be999b50dcf2a9d2ba3dd7
kngwyu <yuji.kngw.80s.revive@gmail.com>
2022-10-18 20:12:37 +0900

    expected_n_children

--- src/emevo/birth_and_death/population.py
+++ src/emevo/birth_and_death/population.py
@@ -4,6 +4,7 @@
 
 from scipy import integrate
 
+from emevo.birth_and_death.birth import BirthFunction
 from emevo.birth_and_death.death import HazardFunction
 from emevo.birth_and_death.statuses import Status
 
@@ -14,7 +15,11 @@ def cumulative_survival(
     energy: float = 0.0,
     max_age: float = 1e6,
 ) -> float:
-    result = integrate.quad(hazard.survival(Status(age=t, energy=energy)), 0, max_age)
+    result = integrate.quad(
+        lambda t: hazard.survival(Status(age=t, energy=energy)),
+        0,
+        max_age,
+    )
     return result[0]
 
 
@@ -26,3 +31,24 @@ def stable_birth_rate(
 ) -> float:
     cumsuv = cumulative_survival(hazard, energy=energy, max_age=max_age)
     return 1.0 / cumsuv
+
+
+def expected_n_children(
+    *,
+    birth: BirthFunction,
+    hazard: HazardFunction,
+    energy: float = 0.0,
+    max_age: float = 1e6,
+    asexual: bool = False,
+) -> float:
+    def integrated(t: int) -> float:
+        status = Status(age=t, energy=energy)
+        if asexual:
+            b = birth.asexual(status)
+        else:
+            b = birth.sexual(status, status)
+        h = hazard.survival(status)
+        return h * b
+
+    result = integrate.quad(integrated, 0, max_age)
+    return result[0]
