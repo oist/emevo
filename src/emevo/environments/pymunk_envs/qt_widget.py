bb870ce5fb1547c1bfff89432d93b2128829daa0
kngwyu <yuji.kngw.80s.revive@gmail.com>
2023-02-15 10:24:06 +0900

    Tweaks on QtWidget

--- src/emevo/environments/pymunk_envs/qt_widget.py
+++ src/emevo/environments/pymunk_envs/qt_widget.py
@@ -14,24 +14,24 @@ def _do_nothing(_env: PymunkEnv) -> None:
     pass
 
 
+def _mgl_qsurface_fmt() -> QSurfaceFormat:
+    fmt = QSurfaceFormat()
+    fmt.setDepthBufferSize(24)
+    fmt.setStencilBufferSize(8)
+    fmt.setVersion(4, 1)
+    fmt.setProfile(QSurfaceFormat.OpenGLContextProfile.CoreProfile)
+    fmt.setSwapBehavior(QSurfaceFormat.SwapBehavior.DoubleBuffer)
+    return fmt
+
+
 class PymunkMglWidget(QOpenGLWidget):
     def __init__(
         self,
         env: PymunkEnv,
         figsize: tuple[float, float] | None = None,
         step_fn: Callable[[PymunkEnv], None] = _do_nothing,
-        voffsets: tuple[int, ...] = (),
-        hoffsets: tuple[int, ...] = (),
         parent: QWidget | None = None,
     ) -> None:
-        # Init widget
-        fmt = QSurfaceFormat()
-        fmt.setDepthBufferSize(24)
-        fmt.setStencilBufferSize(8)
-        fmt.setVersion(4, 1)
-        fmt.setProfile(QSurfaceFormat.OpenGLContextProfile.CoreProfile)
-        fmt.setSwapBehavior(QSurfaceFormat.SwapBehavior.DoubleBuffer)
-        QSurfaceFormat.setDefaultFormat(fmt)
         super().__init__(parent)
         # init renderer
         xlim, ylim = env.get_coordinate().bbox()
@@ -39,17 +39,14 @@ class PymunkMglWidget(QOpenGLWidget):
         y_range = ylim[1] - ylim[0]
         if figsize is None:
             figsize = x_range * 3.0, y_range * 3.0
-        w, h = int(figsize[0]), int(figsize[1])
-        self._figsize = w + int(sum(hoffsets)), h + int(sum(voffsets))
+        self._figsize = int(figsize[0]), int(figsize[1])
         self._make_renderer = partial(
             MglRenderer,
-            screen_width=w,
-            screen_height=h,
+            screen_width=self._figsize[0],
+            screen_height=self._figsize[1],
             x_range=x_range,
             y_range=y_range,
             env=env,
-            voffsets=voffsets,
-            hoffsets=hoffsets,
         )
         self._step_fn = step_fn
         self._env = env
@@ -65,6 +62,7 @@ class PymunkMglWidget(QOpenGLWidget):
 
     def paintGL(self) -> None:
         if not self._initialized:
+            self.context().setFormat(_mgl_qsurface_fmt())
             self._ctx = moderngl.create_context(require=410, share=True, backend="egl")
             if self._ctx.error != "GL_NO_ERROR":
                 raise RuntimeError(f"The following error occured: {self._ctx.error}")
