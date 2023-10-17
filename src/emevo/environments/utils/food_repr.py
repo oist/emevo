baf531b437271ca8df893d2f310d92ae45a2b72e
kngwyu <yuji.kngw.80s.revive@gmail.com>
2023-10-17 15:14:54 +0900

    Test for food num/loc

--- src/emevo/environments/utils/food_repr.py
+++ src/emevo/environments/utils/food_repr.py
@@ -32,13 +32,10 @@ class FoodNumState:
     def appears(self) -> jax.Array:
         return (self.internal - self.current) >= 1.0
 
-    def eaten(self, n: jax.Array) -> Self:
+    def eaten(self, n: int | jax.Array) -> Self:
         return self.replace(current=self.current - n, internal=self.internal - n)
 
-    def fail(self, n: int = 1) -> Self:
-        return self.replace(internal=self.internal - n)
-
-    def recover(self, n: int = 1) -> Self:
+    def recover(self, n: int | jax.Array = 1) -> Self:
         return self.replace(current=self.current + n)
 
 
@@ -54,8 +51,9 @@ class ReprNumConstant:
     initial: int
 
     def __call__(self, state: FoodNumState) -> FoodNumState:
+        internal = jnp.fmax(state.current, state.internal)
         diff = jnp.clip(self.initial - state.current, a_min=0)
-        state = state.replace(internal=state.internal + diff)
+        state = state.replace(internal=internal + diff)
         return state
 
 
@@ -66,7 +64,8 @@ class ReprNumLinear:
 
     def __call__(self, state: FoodNumState) -> FoodNumState:
         # Increase the number of foods by dn_dt
-        internal = jnp.clip(state.internal + self.dn_dt, a_max=float(self.initial))
+        internal = jnp.fmax(state.current, state.internal)
+        internal = jnp.clip(internal + self.dn_dt, a_max=float(self.initial))
         return state.replace(internal=internal)
 
 
@@ -77,8 +76,9 @@ class ReprNumLogistic:
     capacity: float
 
     def __call__(self, state: FoodNumState) -> FoodNumState:
-        dn_dt = self.growth_rate * state.internal * (1 - state.internal / self.capacity)
-        return state.replace(internal=state.internal + dn_dt)
+        internal = jnp.fmax(state.current, state.internal)
+        dn_dt = self.growth_rate * internal * (1 - internal / self.capacity)
+        return state.replace(internal=internal + dn_dt)
 
 
 class ReprNum(str, enum.Enum):
@@ -100,11 +100,11 @@ class ReprNum(str, enum.Enum):
             internal=jnp.array(float(initial)),
         )
         if self is ReprNum.CONSTANT:
-            fn = ReprNumConstant(**kwargs)
+            fn = ReprNumConstant(*args, **kwargs)
         elif self is ReprNum.LINEAR:
-            fn = ReprNumLinear(**kwargs)
+            fn = ReprNumLinear(*args, **kwargs)
         elif self is ReprNum.LOGISTIC:
-            fn = ReprNumLogistic(**kwargs)
+            fn = ReprNumLogistic(*args, **kwargs)
         else:
             raise AssertionError("Unreachable")
         return fn, state
@@ -121,8 +121,8 @@ class ReprLocState:
 ReprLocFn = Callable[[chex.PRNGKey, ReprLocState], jax.Array]
 
 
-def _wrap_initloc(fn: InitLocFn) -> ReprLocState:
-    return lambda key, state: (fn(key), state)
+def _wrap_initloc(fn: InitLocFn) -> ReprLocFn:
+    return lambda key, _: fn(key)
 
 
 class ReprLocSwitching:
@@ -146,11 +146,10 @@ class ReprLocSwitching:
         self,
         key: chex.PRNGKey,
         state: ReprLocState,
-    ) -> tuple[jax.Array, ReprLocState]:
-        count = state.count + 1
+    ) -> jax.Array:
+        count = state.n_produced + 1
         index = (count // self._interval) % self._n
-        result = jax.lax.switch(index, self._locfn_list, key)
-        return result, state.replace(count=count)
+        return jax.lax.switch(index, self._locfn_list, key)
 
 
 class ReprLoc(str, enum.Enum):
