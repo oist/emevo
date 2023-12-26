fd66e42679fdcd7792a84f54c83e598ea27e28b2
kngwyu <yuji.kngw.80s.revive@gmail.com>
2023-12-26 19:34:24 +0900

    Test energy share

--- tests/test_activate.py
+++ tests/test_activate.py
@@ -10,6 +10,8 @@ from emevo.environments.circle_foraging import CFState, CircleForaging
 
 N_MAX_AGENTS = 10
 N_INIT_AGENTS = 5
+ENERGY_SHARE_RATIO = 0.4
+INIT_ENERGY = 20.0
 
 
 @pytest.fixture
@@ -22,6 +24,8 @@ def reset_env(key: chex.PRNGKey) -> tuple[CircleForaging, CFState]:
         "CircleForaging-v0",
         n_max_agents=N_MAX_AGENTS,
         n_initial_agents=N_INIT_AGENTS,
+        init_energy=INIT_ENERGY,
+        energy_share_ratio=ENERGY_SHARE_RATIO,
     )
     state, _ = env.reset(key)
     return typing.cast(CircleForaging, env), state
@@ -45,6 +49,7 @@ def test_deactivate(key: chex.PRNGKey) -> None:
 
 def test_activate(key: chex.PRNGKey) -> None:
     env, state = reset_env(key)
+    init_energy = state.status.energy
     is_parent = jnp.zeros(N_MAX_AGENTS, dtype=bool).at[jnp.array([2, 4, 7])].set(True)
     state, parents = env.activate(state, is_parent)
     expected_active = jnp.array(
@@ -56,3 +61,10 @@ def test_activate(key: chex.PRNGKey) -> None:
     nowhere = jnp.zeros((1, 2))
     is_nowhere = jnp.all(state.physics.circle.p.xy == nowhere, axis=-1)
     chex.assert_trees_all_close(is_nowhere, jnp.logical_not(expected_active))
+    expected_energy = (
+        init_energy.at[jnp.array([2, 4])]
+        .set(INIT_ENERGY * (1.0 - ENERGY_SHARE_RATIO))
+        .at[jnp.array([5, 6])]
+        .set(INIT_ENERGY * ENERGY_SHARE_RATIO)
+    )
+    chex.assert_trees_all_close(state.status.energy, expected_energy)
