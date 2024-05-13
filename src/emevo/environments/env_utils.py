e414dc72fe995367c84bc7ef0a508cf8d789de8e
kngwyu <yuji.kngw.80s.revive@gmail.com>
2024-05-13 17:17:08 +0900

    Change the order of args in ReprNumLinear

--- src/emevo/environments/env_utils.py
+++ src/emevo/environments/env_utils.py
@@ -63,14 +63,14 @@ class ReprNumConstant:
 @dataclasses.dataclass(frozen=True)
 class ReprNumLinear:
     initial: int
+    growth_rate: float
     capacity: float
-    dn_dt: float
 
     def __call__(self, _: int, state: FoodNumState) -> FoodNumState:
         # Increase the number of foods by dn_dt
         internal = jnp.fmax(state.current, state.internal)
         max_value = jnp.array(self.capacity, dtype=jnp.float32)
-        return state._update(jnp.clip(internal + self.dn_dt, a_max=max_value))
+        return state._update(jnp.clip(internal + self.growth_rate, a_max=max_value))
 
 
 @dataclasses.dataclass(frozen=True)
