8927294714f1a0bd4a716a99e33a17fcc064ed0d
kngwyu <yuji.kngw.80s.revive@gmail.com>
2024-08-01 13:36:43 +0900

    Fix clip

--- src/emevo/environments/moderngl_vis.py
+++ src/emevo/environments/moderngl_vis.py
@@ -242,7 +242,7 @@ def _get_clip_ranges(lengthes: list[float]) -> list[tuple[float, float]]:
 
 def _get_sc_color(colors: NDArray, state: State) -> NDArray:
     # Clip labels to make it work when less number of colors are provided
-    label = np.clip(np.array(state.label), min=0, max=len(colors) - 1)
+    label = np.clip(np.array(state.label), a_min=0, a_max=len(colors) - 1)
     default_color = colors[label].astype(np.float32) / 255.0
     inactive_color = np.ones_like(default_color)
     is_active_expanded = np.expand_dims(state.is_active, axis=1)
