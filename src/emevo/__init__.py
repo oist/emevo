d6836deefddd75bb9cbad54497b7f48d072d7b5d
kngwyu <yuji.kngw.80s.revive@gmail.com>
2024-10-24 17:04:17 +0900

    Delete vec2d.py

--- src/emevo/__init__.py
+++ src/emevo/__init__.py
@@ -5,7 +5,6 @@ This package contains API definitions and some environment implementations.
 
 from emevo.env import Env, Status, TimeStep, UniqueID
 from emevo.environments.registry import make, register
-from emevo.vec2d import Vec2d
 from emevo.visualizer import Visualizer
 
 __version__ = "0.1.0"
