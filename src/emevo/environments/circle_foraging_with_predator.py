3d60805f1e7c96437e6b9ad6ad7eff8950cca562
kngwyu <yuji.kngw.80s.revive@gmail.com>
2024-12-12 17:58:46 +0900

    [WIP] Some changes for predator

--- src/emevo/environments/circle_foraging_with_predator.py
+++ src/emevo/environments/circle_foraging_with_predator.py
@@ -1,16 +1,26 @@
 from __future__ import annotations
 
+import functools
 from collections.abc import Iterable
 from dataclasses import replace
-from typing import Any
+from typing import Any, Callable
 
-import chex
 import jax
 import jax.numpy as jnp
 import numpy as np
 from jax.typing import ArrayLike
+from phyjax2d import Circle, Color, Raycast, ShapeDict
 from phyjax2d import Space as Physics
-from phyjax2d import SpaceBuilder, Vec2d, make_approx_circle, make_square
+from phyjax2d import (
+    SpaceBuilder,
+    State,
+    StateDict,
+    Vec2d,
+    circle_raycast,
+    make_approx_circle,
+    make_square,
+    segment_raycast,
+)
 
 from emevo.env import (
     Env,
@@ -22,9 +32,15 @@ from emevo.env import (
     init_uniqueid,
 )
 from emevo.environments.circle_foraging import (
+    AGENT_COLOR,
+    FOOD_COLOR,
+    MAX_ANGULAR_VELOCITY,
+    MAX_VELOCITY,
     CFObs,
     CFState,
     CircleForaging,
+    _get_sensors,
+    _vmap_obs_closest,
     get_tactile,
     init_uniqueid,
     nstep,
@@ -46,93 +62,56 @@ from emevo.environments.env_utils import (
 from emevo.spaces import BoxSpace, NamedTupleSpace
 
 Self = Any
+PREDATOR_COLOR: Color = Color(6, 214, 160)
 
 
-def _make_physics(
-    dt: float,
-    coordinate: CircleCoordinate | SquareCoordinate,
-    linear_damping: float,
-    angular_damping: float,
-    n_velocity_iter: int,
-    n_position_iter: int,
-    n_max_agents: int,
-    n_max_foods: int,
-    agent_radius: float,
-    food_radius: float,
-    obstacles: Iterable[tuple[Vec2d, Vec2d]] = (),
-) -> Physics:
-    builder = SpaceBuilder(
-        gravity=(0.0, 0.0),  # No gravity
-        dt=dt,
-        linear_damping=linear_damping,
-        angular_damping=angular_damping,
-        n_velocity_iter=n_velocity_iter,
-        n_position_iter=n_position_iter,
-        max_velocity=MAX_VELOCITY,
-        max_angular_velocity=MAX_ANGULAR_VELOCITY,
+def _observe_closest(
+    shaped: ShapeDict,
+    p1: jax.Array,
+    p2: jax.Array,
+    stated: StateDict,
+) -> jax.Array:
+    def cr(shape: Circle, state: State) -> Raycast:
+        return circle_raycast(0.0, 1.0, p1, p2, shape, state)
+
+    rc = cr(shaped.circle, stated.circle)
+    to_c = jnp.where(rc.hit, 1.0 - rc.fraction, -1.0)
+    rc = cr(shaped.static_circle, stated.static_circle)
+    to_sc = jnp.where(rc.hit, 1.0 - rc.fraction, -1.0)
+    rc = segment_raycast(1.0, p1, p2, shaped.segment, stated.segment)
+    to_seg = jnp.where(rc.hit, 1.0 - rc.fraction, -1.0)
+    obs = jnp.concatenate(
+        jax.tree_util.tree_map(
+            lambda arr: jnp.max(arr, keepdims=True),
+            (to_c, to_sc, to_seg),
+        ),
     )
-    # Set walls
-    if isinstance(coordinate, CircleCoordinate):
-        walls = make_approx_circle(coordinate.center, coordinate.radius)
-    else:
-        walls = make_square(
-            *coordinate.xlim,
-            *coordinate.ylim,
-            rounded_offset=np.floor(food_radius * 2 / (np.sqrt(2) - 1.0)),
-        )
-    builder.add_chain_segments(chain_points=walls, friction=0.2, elasticity=0.4)
-    for obs in obstacles:
-        builder.add_segment(p1=obs[0], p2=obs[1], friction=0.2, elasticity=0.4)
-    # Prepare agents
-    for _ in range(n_max_agents):
-        builder.add_circle(
-            radius=agent_radius,
-            friction=0.2,
-            elasticity=0.4,
-            density=0.1,
-            color=AGENT_COLOR,
-        )
-    # Prepare foods
-    for _ in range(n_max_foods):
-        builder.add_circle(
-            radius=food_radius,
-            friction=0.2,
-            elasticity=0.4,
-            color=FOOD_COLOR,
-            is_static=True,
-        )
-    return builder.build()
+    return jnp.where(obs == jnp.max(obs, axis=-1, keepdims=True), obs, -1.0)
 
 
-@chex.dataclass
-class StatusWithToxin(Status):
-    toxin: jax.Array
-
-    def deactivate(self, flag: jax.Array) -> Self:
-        return replace(
-            self,
-            age=jnp.where(flag, 0, self.age),
-            toxin=jnp.where(flag, 0, self.toxin),
-        )
+_vmap_obs_closest = jax.vmap(_observe_closest, in_axes=(None, 0, 0, None))
 
 
-def init_status(max_n: int, init_energy: float) -> StatusWithToxin:
-    return StatusWithToxin(
-        age=jnp.zeros(max_n, dtype=jnp.int32),
-        energy=jnp.ones(max_n, dtype=jnp.float32) * init_energy,
-        toxin=jnp.zeros(max_n, dtype=jnp.float32),
-    )
+def get_sensor_obs(
+    shaped: ShapeDict,
+    n_sensors: int,
+    sensor_range: tuple[float, float],
+    sensor_length: float,
+    predator_sensor_length: float,
+    stated: StateDict,
+) -> jax.Array:
+    assert stated.circle is not None
+    p1, p2 = _get_sensors(shaped, n_sensors, sensor_range, sensor_length, stated)
+    return _vmap_obs_closest(shaped, p1, p2, stated)
 
 
 class CircleForagingWithPredator(CircleForaging):
     def __init__(
         self,
         *args,
-        n_max_predator: int = 20,
+        n_max_predators: int = 20,
         predator_radius: float = 20.0,
-        n_predator_sensors: int = 20,
-        n_predator_tactile_bins: int = 8,
-        predator_sensor_length: int = 100.0,
+        predator_sensor_length: int = 100,
         predator_max_force: float = 40.0,
         predator_min_force: float = -20.0,
         predator_init_energy: float = 20.0,
@@ -141,18 +120,93 @@ class CircleForagingWithPredator(CircleForaging):
         predator_basic_ec: float = 0.0,
         **kwargs,
     ) -> None:
+        self._n_max_predators = n_max_predators
+        self._predator_radius = predator_radius
         super().__init__(*args, **kwargs)
 
-    def step(  # type: ignore
+    def _make_sensor_fn(
+        self, observe_food_label: bool
+    ) -> Callable[[StateDict], jax.Array]:
+        if observe_food_label:
+            raise ValueError("Food label in predator env is not supported")
+        else:
+            return jax.jit(
+                functools.partial(
+                    get_sensor_obs,
+                    shaped=self._physics.shaped,
+                    n_sensors=self._n_sensors,
+                    sensor_range=self._sensor_range_tuple,
+                    sensor_length=self._sensor_length,
+                    n_food_labels=None,
+                )
+            )
+
+    def _make_physics(
         self,
-        state: CFState[StatusWithToxin],
-        action: ArrayLike,
-    ) -> tuple[CFState, TimeStep[CFObs]]:
-        # Compute action decay ratio by toxin
-        toxin_decay_rate = 1.0 / (
-            1.0 + self._toxin_alpha * jnp.exp(self._toxin_t0 - state.status.toxin)
+        dt: float,
+        linear_damping: float,
+        angular_damping: float,
+        n_velocity_iter: int,
+        n_position_iter: int,
+        obstacles: Iterable[tuple[Vec2d, Vec2d]] = (),
+    ) -> Physics:
+        builder = SpaceBuilder(
+            gravity=(0.0, 0.0),  # No gravity
+            dt=dt,
+            linear_damping=linear_damping,
+            angular_damping=angular_damping,
+            n_velocity_iter=n_velocity_iter,
+            n_position_iter=n_position_iter,
+            max_velocity=MAX_VELOCITY,
+            max_angular_velocity=MAX_ANGULAR_VELOCITY,
         )
-        toxin_decay = jnp.expand_dims(1.0 - toxin_decay_rate, axis=1)
+        # Set walls
+        if isinstance(self._coordinate, CircleCoordinate):
+            walls = make_approx_circle(self._coordinate.center, self._coordinate.radius)
+        else:
+            walls = make_square(
+                *self._coordinate.xlim,
+                *self._coordinate.ylim,
+                rounded_offset=np.floor(self._food_radius * 2 / (np.sqrt(2) - 1.0)),
+            )
+        builder.add_chain_segments(chain_points=walls, friction=0.2, elasticity=0.4)
+        for obs in obstacles:
+            builder.add_segment(p1=obs[0], p2=obs[1], friction=0.2, elasticity=0.4)
+
+        # Predators
+        for _ in range(self._n_max_predators):
+            builder.add_circle(
+                radius=self._predator_radius,
+                friction=0.2,
+                elasticity=0.4,
+                density=0.1,
+                color=PREDATOR_COLOR,
+            )
+        # Agents
+        for _ in range(self.n_max_agents):
+            builder.add_circle(
+                radius=self._agent_radius,
+                friction=0.2,
+                elasticity=0.4,
+                density=0.1,
+                color=AGENT_COLOR,
+            )
+        # Foods
+        for _ in range(self._n_max_foods):
+            builder.add_circle(
+                radius=self._food_radius,
+                friction=0.2,
+                elasticity=0.4,
+                color=FOOD_COLOR,
+                is_static=True,
+            )
+        return builder.build()
+
+    def step(
+        self,
+        state: CFState[Status],
+        action: ArrayLike,
+    ) -> tuple[CFState[Status], TimeStep[CFObs]]:
         # Add force
         act = jax.vmap(self.act_space.clip)(jnp.array(action))
         f1_raw = jax.lax.slice_in_dim(act, 0, 1, axis=-1)
@@ -160,9 +214,8 @@ class CircleForagingWithPredator(CircleForaging):
         f1 = jnp.concatenate((jnp.zeros_like(f1_raw), f1_raw), axis=1)
         f2 = jnp.concatenate((jnp.zeros_like(f2_raw), f2_raw), axis=1)
         circle = state.physics.circle
-        # Decay force by toxin
-        circle = circle.apply_force_local(self._act_p1, f1 * toxin_decay)
-        circle = circle.apply_force_local(self._act_p2, f2 * toxin_decay)
+        circle = circle.apply_force_local(self._act_p1, f1)
+        circle = circle.apply_force_local(self._act_p2, f2)
         stated = replace(state.physics, circle=circle)
         # Step physics simulator
         stated, solver, nstep_contacts = nstep(
@@ -207,10 +260,7 @@ class CircleForagingWithPredator(CircleForaging):
             self._force_energy_consumption * force_norm + self._basic_energy_consumption
         )
         n_ate = jnp.sum(food_tactile[:, :, self._foraging_indices], axis=-1)
-        # toxin and foods
-        n_ate_foods = n_ate[:, : self._n_food_sources - 1]  # (N-agents, N-foods)
-        n_ate_toxin = n_ate[:, self._n_food_sources - 1]  # (N-agents,)
-        energy_gain = jnp.sum(n_ate_foods * self._food_energy_coef, axis=1)
+        energy_gain = jnp.sum(n_ate * self._food_energy_coef, axis=1)
         energy_delta = energy_gain - energy_consumption
         # Remove and regenerate foods
         key, food_key = jax.random.split(state.key)
@@ -227,11 +277,6 @@ class CircleForagingWithPredator(CircleForaging):
             energy_delta=energy_delta,
             capacity=self._energy_capacity,
         )
-        toxin = jnp.clip(
-            status.toxin + n_ate_toxin * self._toxin_delta - self._toxin_recover_rate,
-            min=0.0,
-        )
-        status = replace(status, toxin=toxin)
         # Construct obs
         obs = CFObs(
             sensor=sensor_obs.reshape(-1, self._n_sensors, self._n_obj),
@@ -249,8 +294,7 @@ class CircleForagingWithPredator(CircleForaging):
                 "energy_consumption": energy_consumption,
                 "n_food_regenerated": n_regen,
                 "n_food_eaten": jnp.sum(eaten, axis=0),  # (N_LABEL,)
-                "n_ate_food": n_ate_foods,  # (N_AGENT, N_LABEL - 1)
-                "n_ate_toxin": n_ate_toxin,  # (N_AGENT,)
+                "n_ate_food": n_ate,  # (N_AGENT, N_LABEL)
             },
         )
         state = CFState(
@@ -265,37 +309,4 @@ class CircleForagingWithPredator(CircleForaging):
             status=status.step(state.unique_id.is_active()),
             n_born_agents=state.n_born_agents,
         )
-        return state, timestep
-
-    def reset(  # type: ignore
-        self,
-        key: chex.PRNGKey,
-    ) -> tuple[CFState[StatusWithToxin], TimeStep[CFObs]]:
-        physics, agent_loc, food_loc, food_num = self._initialize_physics_state(key)
-        N = self.n_max_agents
-        unique_id = init_uniqueid(self._n_initial_agents, N)
-        status = init_status(N, self._init_energy)
-        state = CFState(
-            physics=physics,
-            solver=self._physics.init_solver(),
-            agent_loc=agent_loc,
-            food_loc=food_loc,
-            food_num=food_num,
-            key=key,
-            step=jnp.array(0, dtype=jnp.int32),
-            unique_id=unique_id,
-            status=status,
-            n_born_agents=jnp.array(self._n_initial_agents, dtype=jnp.int32),
-        )
-        sensor_obs = self._sensor_obs(stated=physics)
-        obs = CFObs(
-            sensor=sensor_obs.reshape(-1, self._n_sensors, self._n_obj),
-            collision=jnp.zeros((N, self._n_obj, self._n_tactile_bins), dtype=bool),
-            angle=physics.circle.p.angle,
-            velocity=physics.circle.v.xy,
-            angular_velocity=physics.circle.v.angle,
-            energy=state.status.energy,
-        )
-        # They shouldn't encount now
-        timestep = TimeStep(encount=jnp.zeros((N, N), dtype=bool), obs=obs)
         return state, timestep
