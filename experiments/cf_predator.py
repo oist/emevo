c29e42add68a933521792012cf7289e8baaa442d
kngwyu <yuji.kngw.80s.revive@gmail.com>
2025-03-28 18:05:00 +0900

    Option to select max aggregation for obs reward

--- experiments/cf_predator.py
+++ experiments/cf_predator.py
@@ -63,6 +63,12 @@ def get_mean_sensor_obs(sensor_obs: jax.Array) -> jax.Array:
     return jnp.mean(clipped_sensor_obs, axis=1)  # (N_agents, N_obj)
 
 
+def get_closest_sensor_obs(sensor_obs: jax.Array) -> jax.Array:
+    used_sensor_obs = sensor_obs[:, :, :N_SENSOR_REWARDS]
+    clipped_sensor_obs = jnp.clip(used_sensor_obs, min=0.0)
+    return jnp.max(clipped_sensor_obs, axis=1)  # (N_agents, N_obj)
+
+
 @serde
 @dataclasses.dataclass
 class CfConfigWithPredator(CfConfig):
@@ -95,6 +101,7 @@ def exec_rollout(
     predator_birth_fn: bd.BirthFunction,
     prng_key: jax.Array,
     n_rollout_steps: int,
+    sensor_agg_type: str = "mean",
 ) -> tuple[State, ppo.Rollout, Log, FoodLog, SavedPhysicsState, Obs, jax.Array]:
     n, m = env._n_max_preys, env._n_max_predators  # type: ignore
 
@@ -112,9 +119,12 @@ def exec_rollout(
             env.act_space.sigmoid_scale(actions),  # type: ignore
         )
         obs_t1 = timestep.obs
-        mean_sensor_obs = get_mean_sensor_obs(obs_t1.sensor)
+        if sensor_agg_type == "mean":
+            agg_sensor_obs = get_mean_sensor_obs(obs_t1.sensor)
+        else:
+            agg_sensor_obs = get_closest_sensor_obs(obs_t1.sensor)
         rewards = jnp.expand_dims(
-            reward_fn(timestep.info["n_ate_food"], actions, mean_sensor_obs),
+            reward_fn(timestep.info["n_ate_food"], actions, agg_sensor_obs),
             axis=1,
         )
         rollout = ppo.Rollout(
@@ -175,8 +185,8 @@ def exec_rollout(
             additional_fields={
                 "eaten_preys": dead_eaten,
                 "possible_parents": possible_parents,
-                SENSOR_NAMES[0]: mean_sensor_obs[:, 0],
-                SENSOR_NAMES[1]: mean_sensor_obs[:, 1],
+                SENSOR_NAMES[0]: agg_sensor_obs[:, 0],
+                SENSOR_NAMES[1]: agg_sensor_obs[:, 1],
             },
         )
         foodlog = FoodLog(
@@ -222,6 +232,7 @@ def epoch(
     minibatch_size: int,
     n_optim_epochs: int,
     entropy_weight: float,
+    sensor_agg_type: str = "mean",
 ) -> tuple[
     State, Obs, Log, FoodLog, SavedPhysicsState, optax.OptState, ppo.NormalPPONet
 ]:
@@ -240,6 +251,7 @@ def epoch(
         predator_birth_fn,
         keys[0],
         n_rollout_steps,
+        sensor_agg_type=sensor_agg_type,
     )
     batch = ppo.vmap_batch(rollout, next_value, gamma, gae_lambda)
     opt_state, updated_network = ppo.vmap_update(
@@ -284,6 +296,7 @@ def run_evolution(
     debug_vis_scale: float,
     debug_print: bool,
     headless: bool,
+    sensor_agg_type: str = "mean",
     measure_time: bool = False,
 ) -> None:
     key, net_key, reset_key = jax.random.split(key, 3)
@@ -378,6 +391,7 @@ def run_evolution(
             minibatch_size,
             n_optim_epochs,
             entropy_weight,
+            sensor_agg_type=sensor_agg_type,
         )
         if measure_time:
             now = dt.datetime.now()
@@ -515,6 +529,7 @@ def evolve(
     log_mode: LogMode = LogMode.REWARD_LOG_STATE,
     log_interval: int = 1000,
     savestate_interval: int = 1000,
+    sensor_agg_type: str = "mean",
     debug_vis: bool = False,
     debug_vis_scale: float = 2.0,
     debug_print: bool = False,
@@ -601,6 +616,7 @@ def evolve(
         debug_vis=debug_vis,
         debug_vis_scale=debug_vis_scale,
         headless=headless,
+        sensor_agg_type=sensor_agg_type,
         measure_time=measure_time,
         debug_print=debug_vis or debug_print,
     )
