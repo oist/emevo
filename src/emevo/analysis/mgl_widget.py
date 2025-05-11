64a24da943daa0162cc363a3ac1f848e9ba4876e
kngwyu <yuji.kngw.80s.revive@gmail.com>
2025-05-11 15:54:03 +0900

    [WIP] xysync

--- src/emevo/analysis/mgl_widget.py
+++ src/emevo/analysis/mgl_widget.py
@@ -16,7 +16,7 @@ from PySide6.QtCore import QPointF, Qt, QTimer, Signal, Slot
 from PySide6.QtGui import QColorSpace, QGuiApplication, QMouseEvent, QSurfaceFormat
 from PySide6.QtOpenGLWidgets import QOpenGLWidget
 
-from emevo.environments.circle_foraging import HEAD_COLOR, CircleForaging, CFState
+from emevo.environments.circle_foraging import HEAD_COLOR, CFState, CircleForaging
 from emevo.exp_utils import SavedPhysicsState
 
 
