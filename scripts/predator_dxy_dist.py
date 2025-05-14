ad47c0f6631b0ec9453b928f8cc543fed075f8a4
kngwyu <yuji.kngw.80s.revive@gmail.com>
2025-05-14 16:52:03 +0900

    Lazy loading in dxy_dist

--- scripts/predator_dxy_dist.py
+++ scripts/predator_dxy_dist.py
@@ -1,35 +1,91 @@
 import dataclasses
 from pathlib import Path
+from typing import NamedTuple
 
 import numpy as np
 import polars as pl
 import typer
+from numpy.lib.npyio import NpzFile
 from numpy.typing import NDArray
 
 from emevo.analysis.log_plotting import load_log
 
 
-@dataclasses.dataclass
-class AgentState:
+class AgentState(NamedTuple):
     xy: NDArray
     is_active: NDArray
+    offset: int
 
 
-def load_agent_state(dirpath: Path, n_states: int) -> AgentState:
-    all_xy = []
-    all_is_active = []
+@dataclasses.dataclass
+class AgentStateLoader:
+    size: int
+    files: list[NpzFile]
+    cache: AgentState
+    current_start_index: int = 0
+    current_end_index: int = 0
+
+    def get(self, start: int, end: int) -> AgentState:
+        start_index = start // self.size
+        end_index = end // self.size
+        if (
+            self.current_start_index <= start_index
+            and end_index <= self.current_end_index
+        ):
+            return self.cache
+        # If not, extend the cache
+        else:
+            self._extend_cache(start_index, end_index)
+            return self.cache
+
+    def _extend_cache(self, start_index: int, end_index: int) -> None:
+        xy_next = self.files[end_index]["circle_axy"].astype(np.float32)[:, :, 1:]
+        is_active_next = self.files[end_index]["circle_is_active"].astype(bool)
+        self.current_end_index = end_index
+        if self.current_start_index < start_index:
+            # Drop
+            if start_index == end_index:
+                xy = xy_next
+                is_active = is_active_next
+            else:
+                print(self.cache.xy.shape, xy_next.shape)
+                xy = np.concatenate((self.cache.xy[start_index * self.size :], xy_next))
+                is_active = np.concatenate(
+                    (self.cache.is_active[start_index * self.size :], is_active_next)
+                )
+            self.current_start_index = start_index
+            print("Extend and drop")
+        else:
+            xy = np.concatenate((self.cache.xy, xy_next), axis=0)
+            is_active = np.concatenate((self.cache.is_active, is_active_next), axis=0)
+            print("Extend")
+        self.cache = AgentState(
+            xy=xy,
+            is_active=is_active,
+            offset=start_index * self.size,
+        )
+
+
+def get_state_loader(
+    dirpath: Path,
+    n_states: int,
+    size: int = 1024000,
+) -> AgentStateLoader:
+    files = []
     for i in range(n_states):
         npzfile = np.load(dirpath / f"state-{i + 1}.npz")
-        all_xy.append(npzfile["circle_axy"].astype(np.float32)[:, :, 1:])
-        all_is_active.append(npzfile["circle_is_active"].astype(bool))
-    return AgentState(
-        xy=np.concatenate(all_xy),
-        is_active=np.concatenate(all_is_active),
+        files.append(npzfile)
+    xy = files[0]["circle_axy"].astype(np.float32)[:, :, 1:]
+    is_active = files[0]["circle_is_active"].astype(bool)
+    return AgentStateLoader(
+        size=size, files=files, cache=AgentState(xy=xy, is_active=is_active, offset=0)
     )
 
 
-def load(logd: Path, n_states: int = 10) -> tuple[AgentState, pl.DataFrame]:
-    agent_state = load_agent_state(logd, n_states)
+def load(
+    logd: Path, n_states: int = 10, state_size: int = 1024000
+) -> tuple[AgentStateLoader, pl.DataFrame]:
+    state_loader = get_state_loader(logd, n_states, state_size)
 
     eaten_path = logd / "eaten.parquet"
     if eaten_path.exists():
@@ -52,7 +108,7 @@ def load(logd: Path, n_states: int = 10) -> tuple[AgentState, pl.DataFrame]:
             )
             .collect()
         )
-    return agent_state, stepdf
+    return state_loader, stepdf
 
 
 def mean_masked_norm(a: NDArray, b: NDArray, mask: NDArray):
@@ -61,15 +117,16 @@ def mean_masked_norm(a: NDArray, b: NDArray, mask: NDArray):
 
 
 def compute_dxy_dist(
-    agent_state: AgentState,
+    state_loader: AgentStateLoader,
     stepdf: pl.DataFrame,
     n_max_preys: int,
     n_max_iter: int | None = None,  # For debugging
 ) -> pl.DataFrame:
     def dxy_dist(start: int, end: int, slot: int) -> tuple[NDArray, NDArray]:
-        xy_selected = agent_state.xy[start : end + 1, slot]
-        prey_xy = agent_state.xy[start : end + 1, :n_max_preys]
-        predator_xy = agent_state.xy[start : end + 1, n_max_preys:]
+        xy, is_active, offset = state_loader.get(start, end)
+        xy_selected = xy[start - offset : end - offset + 1, slot]
+        prey_xy = xy[start - offset : end - offset + 1, :n_max_preys]
+        predator_xy = xy[start - offset : end - offset + 1, n_max_preys:]
         # dxy
         dxy_self = xy_selected[1:] - xy_selected[:-1]  # (N - 1, 2)
         dxy_self_expanded = np.expand_dims(dxy_self, axis=1)
@@ -77,13 +134,14 @@ def compute_dxy_dist(
         dxy_predator = predator_xy[1:] - predator_xy[:-1]  # (N - 1, M2, 2)
         # mask
         prey_mask = np.logical_and(
-            agent_state.is_active[start + 1 : end + 1, :n_max_preys],
-            agent_state.is_active[start:end, :n_max_preys],
+            is_active[start - offset + 1 : end - offset + 1, :n_max_preys],
+            is_active[start - offset : end - offset, :n_max_preys],
         )
         predator_mask = np.logical_and(
-            agent_state.is_active[start + 1 : end + 1, n_max_preys:],
-            agent_state.is_active[start:end, n_max_preys:],
+            is_active[start - offset + 1 : end - offset + 1, n_max_preys:],
+            is_active[start - offset : end - offset, n_max_preys:],
         )
+        print(dxy_self_expanded.shape, dxy_prey.shape, prey_mask.shape)
         to_prey = mean_masked_norm(dxy_self_expanded, dxy_prey, prey_mask)
         to_predator = mean_masked_norm(dxy_self_expanded, dxy_predator, predator_mask)
         return to_prey, to_predator
@@ -91,7 +149,7 @@ def compute_dxy_dist(
     uid_list = []
     prey_list = []
     predator_list = []
-    for i, (uid, slot, start, end) in enumerate(stepdf.iter_rows()):
+    for i, (uid, slot, start, end) in enumerate(stepdf.sort("start").iter_rows()):
         if n_max_iter is not None and n_max_iter < i:
             break
         if end - start < 2:
@@ -113,9 +171,14 @@ def compute_dxy_dist(
     )
 
 
-def main(logd: Path, n_states: int = 10, n_max_preys: int = 450) -> None:
-    agent_state, stepdf = load(logd, n_states)
-    avgd_df = compute_dxy_dist(agent_state, stepdf, n_max_preys)
+def main(
+    logd: Path,
+    n_states: int = 10,
+    n_max_preys: int = 450,
+    state_size: int = 1024000,
+) -> None:
+    agent_state, stepdf = load(logd, n_states, state_size)
+    avgd_df = compute_dxy_dist(agent_state, stepdf, n_max_preys, 20)
     avgd_df.write_parquet(logd / "avg-movement-dist.parquet")
 
 
