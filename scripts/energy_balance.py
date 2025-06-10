9e8641314815a2c1ab963431736863dfbf2bccb0
kngwyu <yuji.kngw.80s.revive@gmail.com>
2025-06-10 16:33:00 +0900

    [script] More info in energy_balance.py

--- scripts/energy_balance.py
+++ scripts/energy_balance.py
@@ -9,12 +9,26 @@ from emevo.analysis.log_plotting import load_log
 def main(logd: Path, n_states: int = 10, bin_length: int = 1000) -> None:
     logdf = load_log(logd, last_idx=n_states)
     age_df = logdf.with_columns(
-        (pl.col("step") - pl.col("step").min().over("unique_id")).alias("age")
+        (pl.col("step") - pl.col("step").min().over("unique_id")).alias("age"),
+        pl.col("energy").diff().alias("Energy Delta"),
+    )
+    age_ed_df = age_df.with_columns(
+        pl.when(pl.col("age") == 0)
+        .then(pl.col("energy_gain") - pl.col("consumed_energy"))
+        .otherwise(pl.col("energy_delta"))
+        .alias("energy_delta"),
     )
     balance_df = (
-        age_df.group_by("unique_id", ((pl.col("age") // bin_length) * bin_length).alias("age_bin"))
+        age_ed_df.group_by(
+            "unique_id", ((pl.col("age") // bin_length) * bin_length).alias("age_bin")
+        )
         .agg(
-            (pl.col("energy_gain").sum() / pl.col("consumed_energy").sum()).alias("Energy Balance")
+            pl.col("energy_gain").mean(),
+            pl.col("consumed_energy").mean(),
+            (pl.col("energy_gain") - pl.col("consumed_energy") - pl.col("energy_delta"))
+            .mean()
+            .alias("repr_energy_consumption"),
+            pl.col("reward").mean(),
         )
         .sort("unique_id", "age_bin")
         .collect()
