e8489bcf4181a18c2131ccc8255d3dec6ec4e4f3
kngwyu <yuji.kngw.80s.revive@gmail.com>
2025-11-06 12:16:40 +0900

    [script] Use df.len instead of count

--- scripts/popl.py
+++ scripts/popl.py
@@ -8,7 +8,7 @@ from emevo.analysis.log_plotting import load_log
 
 def main(logd: Path, n_states: int = 10) -> None:
     ldf = load_log(logd, last_idx=n_states).with_columns(pl.col("step").alias("Step"))
-    pop_df = ldf.group_by("Step").agg(pl.count().alias("Population"))
+    pop_df = ldf.group_by("Step").agg(pl.len().alias("Population"))
     pop_df.collect().write_parquet(logd / "popl.parquet")
 
 
