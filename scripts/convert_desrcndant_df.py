bdab7344e1507c82c2b9fc0146d5a4c0fe84a3ce
kngwyu <yuji.kngw.80s.revive@gmail.com>
2026-03-04 17:20:34 +0900

    [script] Preserve predator color

--- scripts/convert_desrcndant_df.py
+++ scripts/convert_desrcndant_df.py
@@ -4,6 +4,8 @@ import polars as pl
 import seaborn as sns
 import typer
 
+from emevo.environments.circle_foraging_with_predator import PREDATOR_COLOR
+
 
 def main(dfile: Path) -> None:
     palette = sns.color_palette()
@@ -17,22 +19,37 @@ def main(dfile: Path) -> None:
     if column_name is None:
         raise RuntimeError("No ancestor column found")
 
+    RGB = "RGB"
+
+    if "kind" in df.columns:
+
+        def color_logic(index: int):
+            return (
+                pl.when(pl.col("kind") == 0)
+                .then(
+                    pl.when(pl.col(column_name))
+                    .then(palette[1][index] * 255)
+                    .otherwise(palette[0][index] * 255)
+                    .cast(pl.Int32)
+                )
+                .otherwise(PREDATOR_COLOR[index])
+                .alias(RGB[index])
+            )
+    else:
+
+        def color_logic(index: int):
+            return (
+                pl.when(pl.col(column_name))
+                .then(palette[1][index] * 255)
+                .otherwise(palette[0][index] * 255)
+                .alias(RGB[index])
+                .cast(pl.Int32)
+            )
+
     df = df.with_columns(
-        pl.when(pl.col(column_name))
-        .then(palette[0][0] * 255)
-        .otherwise(palette[1][0] * 255)
-        .alias("R")
-        .cast(pl.Int32),
-        pl.when(pl.col(column_name))
-        .then(palette[0][1] * 255)
-        .otherwise(palette[1][1] * 255)
-        .alias("G")
-        .cast(pl.Int32),
-        pl.when(pl.col(column_name))
-        .then(palette[0][2] * 255)
-        .otherwise(palette[1][2] * 255)
-        .alias("B")
-        .cast(pl.Int32),
+        color_logic(0),
+        color_logic(1),
+        color_logic(2),
         pl.lit(255).alias("A"),
     )
     df = df.select("unique_id", "R", "G", "B", "A")
